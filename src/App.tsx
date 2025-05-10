90f416a5e7e8a793013a1e97424f52643f48bef5
willibrown12 <viliking123@gmail.com>
2025-05-11 02:34:42 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { motion } from "framer-motion";
+import PulsingButton from './componets/pulsingButton'
+
+
+function App() {
+  return (
+    
+      <div>
+   <PulsingButton/>
+      </div>
+  
+  );
+}
+
+export default App;
---
+++ src/componets/pulsingButton.tsx
@@ -0,0 +1,13 @@
+import { motion } from "framer-motion";
+
+
+const PulsingButton = () => {
+
+
+return ( 
+    
+    <motion.button     className="px-6 py-2 text-white bg-blue-500 rounded outline-none"> click me</motion.button>
+
+)}
+
+export default PulsingButton
